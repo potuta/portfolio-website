33481b4af4ba6e6e58b0448a4f2834f7010b39c9
potuta <cyrilbautista2@gmail.com>
2026-06-01 18:00:04 +0800

    fixed import

--- components/ui/particles.tsx
+++ components/ui/particles.tsx
@@ -2,7 +2,7 @@
 
 import type React from "react"
 import { useEffect, useRef, useState } from "react"
-import { cn } from "~/lib/utils"
+import { cn } from "@/lib/utils"
 
 interface MousePosition {
   x: number
